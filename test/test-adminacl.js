7ad858b912bb1fd026b8bc22e081adc45e48c423
cicorias <github@cicoria.com>
2016-02-26 10:12:31 -0500

    add more tests and change getSecret to getHeader fixing singleton issue move to min pouch on sample

--- test/test-adminacl.js
+++ test/test-adminacl.js
@@ -4,30 +4,28 @@ var request = require('supertest'),
   express = require('express'),
   path = require('path');
 
-var app = express();
-
 var adminAcl = require(path.join(__dirname, '../lib/index'));
-
-app.use(adminAcl.isAdminOk);
-
-app.get('/', function (req, res) {
-  console.log('in the handler for get');
-  res.status(200).json({ message: 'you made it' });
-});
-
-app.post('/', function (req, res) {
-  res.status(200).json({ name: 'you made it' });
-});
 
 describe('basic authn tests', function () {
 
+  var app;
   before(function () {
-    adminAcl.setSecret('foobar');
-    // app.set('adminAclSecret', 'foobar');
+    app = express();
+
+    app.use(adminAcl.acl({ secret: 'foobar' }));
+    app.get('/', function (req, res) {
+      console.log('in the handler for get');
+      res.status(200).json({ message: 'you made it' });
+    });
+
+    app.post('/', function (req, res) {
+      console.log('in the post handler');
+      res.status(200).json({ name: 'you made it' });
+    });
   })
 
-  describe('no header tests', function () {
-    describe('GET / with no header', function () {
+  describe('no header tests', function (done) {
+    describe('GET / with no header', function (done) {
       it('respond with an access denied - 401', function (done) {
         request(app)
           .get('/')
@@ -37,7 +35,7 @@ describe('basic authn tests', function () {
       })
     })
 
-    describe('POST / with no header', function () {
+    describe('POST / with no header', function (done) {
       it('respond with an access denied - 401', function (done) {
         request(app)
           .post('/')
@@ -121,5 +119,94 @@ describe('basic authn tests', function () {
 
   })
 
+  describe('crypto authn tests - caller lacks CLEAR in header', function () {
+
+    describe('simple Authn with binary call', function () {
+      it('respond with an 401 - unauthorized', function (done) {
+        request(app)
+          .post('/')
+          .set('Authorization', 'foobar')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(401, done);
+      })
+    })
+
+
+    describe('simple Authn with binary call', function () {
+      it('respond with an 401 - unauthorized', function (done) {
+        request(app)
+          .post('/')
+          .set('Authorization', 'foobar')
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(401, done);
+      })
+    })
+
+  })
+
+  describe('some tests with a req.body', function () {
+    var secret = 'truk653Dimohr/0D7IDuPg==THISDOESNTMATTER';
+
+    var app;
+    before(function () {
+      app = express();
+
+      app.use(adminAcl.acl({ secret: secret }));
+      app.get('/', function (req, res) {
+        console.log('in the handler for get');
+        res.status(200).json({ message: 'you made it' });
+      });
+
+      app.post('/', function (req, res) {
+        console.log('in the post handler');
+        res.status(200).json({ name: 'you made it' });
+      });
+    })
+
+    it('should reject as invalid header', function (done) {
+      request(app)
+        .post('/')
+        .send({ name: 'Manny', species: 'cat' })
+        .set('Authorization', 'foobar')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(401, done);
+    })
+
+    it('should reject as invalid header', function (done) {
+      request(app)
+        .get('/')
+        .send({ name: 'Manny', species: 'cat' })
+        .set('Authorization', 'foobar')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(401, done);
+    })
+
+    it('should OK as valid header', function (done) {
+      request(app)
+        .post('/')
+        .send({ name: 'Manny', species: 'cat' })
+        .set('Authorization', 'CLEAR ' + secret)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+
+
+    it('should OK as valid header', function (done) {
+      request(app)
+        .get('/')
+        .send({ name: 'Manny', species: 'cat' })
+        .set('Authorization', 'CLEAR ' + secret)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    })
+
+
+  })
 
 })
